3a769f240338e74c5b664d1d688df4d46a993c8c
kylemaxwellcollins <kylemaxwellcollins@gmail.com>
2018-12-19 12:40:28 -0500

    added burger menu for mobile :wrench:

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,12 +1,28 @@
 AOS.init({
-  disable: 'mobile'
+  disable: "mobile"
+});
 
-}
-);
+(function() {
+  var hamburger = {
+    navToggle: document.querySelector(".nav-toggle"),
+    nav: document.querySelector("nav"),
 
+    doToggle: function(e) {
+      e.preventDefault();
+      this.navToggle.classList.toggle("expanded");
+      this.nav.classList.toggle("expanded");
+    }
+  };
+
+  hamburger.navToggle.addEventListener("click", function(e) {
+    hamburger.doToggle(e);
+  });
+})();
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function () { fixNav() };
+window.onscroll = function() {
+  fixNav();
+};
 
 // Get the navbar
 let navbar = document.getElementById("nav");
@@ -17,10 +33,8 @@ let sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function fixNav() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("fixNav")
+    navbar.classList.add("fixNav");
   } else {
     navbar.classList.remove("fixNav");
   }
-} 
-
-
+}
